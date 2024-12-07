0ed1d779a78517ec91a74c2a99ee7387bc53b8d4
Henze1 <podratchyanhenzel@gmail.com>
2024-12-08 00:51:38 +0400

    some changes

--- main.c
+++ main.c
@@ -11,33 +11,55 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
+#include <stdlib.h>
 
 #define LEN 4
 
-void	split_argv(char *str, int *arr_up, int *arr_down, int *arr_left, int *arr_right);
-int	**create_matrix(void);
-void	delete_matrix(int **mat);
+void	split_argv(char *str, int *arr);
+int	**create_mat(void);
+void    init_up_down(int *arr, int *arr_up, int *arr_down);
+void    init_left_right(int *arr, int *arr_left, int *arr_right);
+void    dest_mat(int **mat);
+void	service(int *arr);
+void	print_mat(int **mat);
 
 int	main(int argc, char *argv[])
 {
-	int	arr_up[LEN];
-	int	arr_down[LEN];
-	int	arr_left[LEN];
-	int	arr_right[LEN];
+    int	arr[16];
 
-	split_argv(argv[1], arr_up, arr_down, arr_left, arr_right);
-
+    if (argc != 2)
+    {
+        write(2, "Invalid input\n", 14);
+        exit (0);
+    }
+    split_argv(argv[1], arr);
+	service(arr);
 	return (0);
 }
 
-//this function takes the string passed to argv[1] and splits it to craete a matrix
-void	split_argv(char *str, int *arr_up, int *arr_down, int *arr_left, int *arr_right)
+void	service(int *arr)
 {
-	int	arr[16];
+	int	arr_up[4];
+	int	arr_down[4];
+	int	arr_left[4];
+	int	arr_right[4];
+	int	**mat;
+
+	init_up_down(arr, arr_up, arr_down);
+	init_left_right(arr, arr_left, arr_right);
+	mat = create_mat();
+
+	print_mat(mat);
+
+	dest_mat(mat);
+}
+
+//this function takes the string passed to argv[1] and splits it to craete a matrix
+void    split_argv(char *str, int *arr)
+{
 	int	i;
 
-	if (str == '\0')
+	if (*str == '\0')
 		return ;
 	i = 0;
 	while (*str)
@@ -50,38 +72,87 @@ void	split_argv(char *str, int *arr_up, int *arr_down, int *arr_left, int *arr_r
 		else if (*str != ' ')
 		{
 			write(2, "Invalid input\n", 14);
-			return ;
+			exit (0);
 		}
 		str++;
 	}
-	i = 0;
-	while (i < LEN)
-	{
-		*(arr_up + i) = arr[i];
-		*(arr_down + i) = arr[i + 4];
-		*(arr_left + i) = arr[i + 8];
-		*(arr_right + i) = arr[i + 12];
-		++i;
-	}
 }
 
-int	**create_matrix(void)
+void    init_up_down(int *arr, int *arr_up, int *arr_down)
+{
+    int i;
+
+    i = 0;
+    while (i < 4)
+    {
+        arr_up[i] = arr[i];
+        arr_down[i] = arr[i + 4];
+    	++i;
+    }
+}
+
+void    init_left_right(int *arr, int *arr_left, int *arr_right)
+{
+    int i;
+
+    i = 0;
+    while (i < 4)
+    {
+        arr_left[i] = arr[i + 8];
+        arr_right[i] = arr[i + 12];
+    	++i;
+    }
+}
+
+int	**create_mat(void)
+{
+    int i;
+    int**   arr;
+
+    i = 0;
+    arr = (int**)malloc(4 * sizeof(int*));
+    while (i < 4)
+    {
+        arr[i] = (int*)malloc(4 * sizeof(int));
+        ++i;
+    }
+	return (arr);
+}
+
+void    dest_mat(int **mat)
+{
+    int i;
+
+    i = 0;
+    while (i < 4)
+    {
+        free(mat[i]);
+    	mat[i] = NULL;
+    	++i;
+    }
+
+    free(mat);
+	mat = NULL;
+}
+
+void	print_mat(int **mat)
 {
 	int	i;
-    int j;
-	int mat[4][4];
+	int j;
+	char num;
 
 	i = 0;
-    j = 0;
-    while (i < r)
+	j = 0;
+	while (i < 4)
 	{
-        while (j < r)
-        {
-            mat[i][j] = 0;
-            ++j;
-        }
+		while (j < 4)
+		{
+			num = mat[i][j] + '0';
+			write(1, &num, 1);
+			++j;
+		}
+		write(1, "\n", 1);
+		j = 0;
 		++i;
 	}
-
-	return (mat);
 }
